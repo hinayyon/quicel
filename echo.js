752d65fec30958427e4e42f690d2171fa4d21716
hinayyon <admin+4@hinata.in>
2012-01-23 00:54:47 +0900

    Update echo.js

--- echo.js
+++ echo.js
@@ -1,5 +1,5 @@
 /* command: echo
- * summary: 渡された引数をそのまま文字列として返します。
+ * summary: 渡された引数をそのまま文字列として返します
  *   usage: echo hoge        # => "hoge"
  *          echo hoge hoge   # => "hoge hoge"
  *          echo "hoge hoge" # => "hoge hoge"
