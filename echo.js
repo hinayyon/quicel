e6e13ac585bf6a7a69279fe0bfb77392285e2f56
hinayyon <admin+4@hinata.in>
2012-01-23 00:38:59 +0900

    説明つけた

--- echo.js
+++ echo.js
@@ -1,1 +1,8 @@
-return params.join("mogmog");
+/* command: echo
+ * summary: 引数を文字列として返します。
+ *   usage: echo hoge => "hoge"
+ *          echo hoge hoge => "hoge hoge"
+ *          echo "hoge hoge" => "hoge hoge"
+ */
+ 
+return params.join(" ");
